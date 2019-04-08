f07d999b447a49a52ff77fc41015d09c13605f0b
Jessica Welch <jessicawelch@knights.ucf.edu>
2019-04-07 22:13:20 -0400

    Space Shooter Extended

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject [] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text restartText;
+    public Text gameOverText;
+    public Text ScoreText;
+    public Text winText;
+
+    private bool gameOver;
+    private bool restart;
+    private int points;
+
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        winText.text = "";
+        restartText.text = "";
+        gameOverText.text = "";
+
+        points = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+    }
+
+    private void Update()
+    {
+        if(restart)
+        {
+            if(Input.GetKeyDown (KeyCode.P))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+
+
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'P' for Play Again";
+                restart = true;
+                break;
+            }
+        }
+    
+    }
+    public void AddScore(int newScoreValue)
+    {
+        points += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + points;
+
+        if (points >= 100)
+        {
+            winText.text = "Game Created by Jessica Welch";
+            gameOver = true;
+            restart = true;
+        }
+    }
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
+
+    }
+
+    
+}
